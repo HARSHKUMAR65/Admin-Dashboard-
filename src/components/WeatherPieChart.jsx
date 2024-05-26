72e040f2f5a78c7cb72db7cd6a9a25a9cbf54253
Harsh yaduvanshi <kabiryadav55555@gmail.com>
2024-05-26 14:51:17 +0530

    Fetch Data From kaggle Through API

--- src/components/WeatherPieChart.jsx
+++ src/components/WeatherPieChart.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import { PieChart, Pie, Tooltip, ResponsiveContainer, Legend } from 'recharts';
+import { PieChart, Pie, Tooltip, ResponsiveContainer, Legend, Line } from 'recharts';
 import Banglore from '../Mocks/Banglore';
 
 const WeatherPieChart = () => {
     return (
         <div className="w-4/5 mx-auto p-6 rounded-lg shadow-lg bg-white">
-            <h1 className="text-center text-2xl font-semibold text-gray-700 mb-4">City Weather Data</h1>
-            <ResponsiveContainer width="100%" height={400}>
+            <h1 className="text-center text-2xl font-semibold text-gray-700 mb-4">Humidity and Max Temperature</h1>
+            <ResponsiveContainer width="100%" height={300}>
                 <PieChart>
                     <Pie
                         data={Banglore}
                         dataKey="humidity"
-                        isAnimationActive={false}
-                        // nameKey="city"
+                        // isAnimationActive={false}
+                        nameKey="city"
                         cx="40%"
                         cy="50%"
                         outerRadius={100}
@@ -21,9 +21,13 @@ const WeatherPieChart = () => {
                         label
                     >
                     </Pie>
+
+                    <Tooltip />
+                    <Line ype="monotone" dataKey="maxTemp" stroke="#8884d8" activeDot={{ r: 8 }}/>
                     <Pie
                         data={Banglore}
                         dataKey="maxTemp"
+                        isAnimationActive={false}
                         // nameKey="city"
                         cx="55%"
                         cy="50%"
@@ -34,7 +38,7 @@ const WeatherPieChart = () => {
                     >
                     </Pie>
                     <Tooltip />
-                    <Legend />
+                    <Line ype="monotone" dataKey="maxTemp" stroke="#8884d8"/>
                 </PieChart>
             </ResponsiveContainer>
         </div>
